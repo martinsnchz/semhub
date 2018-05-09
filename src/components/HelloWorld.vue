f83aa4ed65f6c4191472fea3213e216705328783
matteomnr <matteo.mnr@eduge.ch>
2018-05-09 10:52:52 +0200

    site.vue ajouté

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Menu</h2>
+    <ul>
+      <li>
+        <a
+          href="#/enfance"
+          target="_blank"
+        >
+          Enfance des frères
+        </a>
+      </li>
+      <li>
+        <a
+          href="/"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="/"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="h"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>gljhgljkg</h2>
+    <ul>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href=""
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Helloworld',
+  data () {
+    return {
+      msg: 'Le musée des Frères Wright'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Site.vue
@@ -0,0 +1,287 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8">
+
+<script src="https://unpkg.com/vue"></script>
+
+<h1>Le musée des Frères Wright</h1>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+
+<style>
+
+body {
+    background-image: url( "https://78.media.tumblr.com/6e9049e02a6a9eb0d87fdf1de96a5366/tumblr_inline_o67yh3sJiw1sa90vo_540.jpg");
+}
+
+h1 {
+    color: #333300;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 40px;
+    border-style: none;
+    background-color: #ffffff;
+
+}
+
+p.article {
+    color: #e6fff9;
+    font-family: Arial ;
+    text-align: justify;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    line-height: 1.2 ;
+
+
+
+}
+
+art1 {
+    color: #ffffff;
+    font-family: Arial ;
+    text-align: justify;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+    line-height: 1.2 ;
+    text-decoration: line-through;
+
+
+
+
+
+}
+
+h3 {
+    color: #666633;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 20px;
+    border-style: none;
+
+
+}
+
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 8px;
+    border: dotted;
+}
+
+
+.navbar {
+    overflow: hidden;
+    background-color: #666633;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.navbar a {
+    float: left;
+    font-size: 16px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+.dropdown {
+    float: left;
+    overflow: hidden;
+}
+
+.dropdown .dropbtn {
+    cursor: pointer;
+    font-size: 16px;
+    border:none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+}
+
+.navbar a:hover, .dropdown:hover .dropbtn, .dropbtn:focus {
+    background-color:  #271a0c;
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+
+}
+
+.dropdown-content a {
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+
+}
+
+.dropdown-content a:hover {
+    background-color: #ddd;
+}
+
+.show {
+    display: block;
+}
+
+
+
+div.transbox {
+    margin: 40px;
+    background-color: #000000;
+    border: none;
+    opacity: 0.5;
+    filter: alpha(opacity=60); /* For IE8 and earlier */
+    outline: 15px ;
+    outline-color: #ffffff ;
+    outline-offset: 15px;
+    padding: 30px;
+
+}
+
+div.transbox art1 {
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: justify;
+    font-size: 18px;
+    border: 15px ;
+    border-style: none;
+    outline-offset: 25px;
+    margin: 100%;
+    font-weight: none;
+
+
+
+}
+
+.inner-border {
+    background: #000;
+    color: #fff;
+    margin: 50px;
+    padding: 20px;
+    position: relative;
+    opacity: 0.7;
+    filter: alpha(opacity=60); /* For IE8 and earlier */
+}
+.inner-border:before {
+    border: 5px solid #000;
+    content: "";
+    position: absolute;
+    top: -10px;
+    bottom: -10px;
+    left: -10px;
+    right: -10px;
+}
+
+span {
+    float: left;
+    width: 0.7em;
+    font-size: 400%;
+    font-family: algerian, courier;
+    line-height: 80%;
+}
+
+</style>
+</head>
+
+
+
+<body>
+
+<div class="navbar">
+  <a href="#home">Home</a>
+  <a href="#Introduction">Introduction</a>
+
+  <div class="dropdown">
+    <button class="dropbtn" onclick="myFunction()">Chronologie
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content" id="myDropdown">
+      <a href="#">Enfance</a>
+      <a href="#">Débuts</a>
+      <a href="#">Premières Inventions</a>
+    </div>
+  </div>
+  <a href="#chronologie">Gallerie</a>
+  <a href="#chronologie">Vidéos</a>
+  <a href="#chronologie">Sources</a>
+
+</div>
+
+
+<h3>BIENVENUE A NOTRE SITE</h3>
+
+<img src="http://www.thewrightbrothers.org/owwwport.jpg" alt="Freres" style=" width:460px ">
+
+<div class="inner-border">
+    <p class=article>
+    <br/>
+
+    Ceci est un site créé en collaboration avec Martin Sanchez, Jules Boychenko et Long Nyugen.
+    Il vise à informer nos chers lecteurs de l'histoire derrière les premiers pionniers de l'aviation; Les Frères Wright. On éspère que vous preneriez du plaisir à nous lire.
+    <br/>
+    <br/>
+    Les frères Orville Wright (19 août 1871 - 30 janvier 1948) et Wilbur Wright (16 avril 1867 - 30 mai 1912) sont deux célèbres pionniers américains de l'aviation, à la fois chercheurs1, ingénieurs, concepteurs, constructeurs et pilotes.
+<br/>
+Après des vols de mise au point sur planeurs, ils affirment avoir effectué en 1903 leur premier vol motorisé. Ils se sont distingués de leurs prédécesseurs et de leurs contemporains par leur approche analytique et expérimentale du problème. Leur contribution essentielle sera d'avoir correctement analysé la mécanique de vol du virage et d'avoir réalisé, en 1902, les premiers vols contrôlés grâce au couplage de la gouverne de direction et du gauchissement (obtenu par vrillage) des ailes. Maîtrisant le pilotage, ils effectuent en 1905 les premiers vols pouvant être qualifiés de « stables », de longue durée, avec des virages inclinés et non dérapés. Cependant, leur obsession du secret autour de leurs machines et de leurs capacités à réaliser un vol motorisé contrôlé (qu'ils maintiendront presque totalement jusqu'à l'obtention de brevet d'invention en 1905) entraînera un scepticisme général, en particulier en Europe, quand ils commenceront à communiquer en 1905 tout en exigeant un contrat commercial ferme avant toute démonstration. Cela explique le décalage de plusieurs années entre les premiers vols contrôlés de 1905, à l'écart de tout spectateur dans les dunes de Caroline du Nord, et les vols publics de 1908 où leur maîtrise du pilotage sera reconnue. Consacrant leur énergie à protéger leur invention et à des luttes de brevets, ils ne remettent pas en cause la configuration atypique de leur machine (configuration canard, pas d'ailerons, pas de roues, hélices à l'arrière), qui est obsolète en 1910 et ne sera pas poursuivie. <br/><br/><br/>
+</p>
+</div>
+
+
+<div class="background">
+  <div class="transbox">
+    <art1>
+    <br/><br/><br/>
+
+   <span> L</span> es frères Orville Wright (19 août 1871 - 30 janvier 1948) et Wilbur Wright (16 avril 1867 - 30 mai 1912) sont deux célèbres pionniers américains de l'aviation, à la fois chercheurs1, ingénieurs, concepteurs, constructeurs et pilotes.
+<br/>
+<br/>
+Après des vols de mise au point sur planeurs, ils affirment avoir effectué en 1903 leur premier vol motorisé. Ils se sont distingués de leurs prédécesseurs et de leurs contemporains par leur approche analytique et expérimentale du problème. Leur contribution essentielle sera d'avoir correctement analysé la mécanique de vol du virage et d'avoir réalisé, en 1902, les premiers vols contrôlés grâce au couplage de la gouverne de direction et du gauchissement (obtenu par vrillage) des ailes.
+
+Maîtrisant le pilotage, ils effectuent en 1905 les premiers vols pouvant être qualifiés de « stables », de longue durée, avec des virages inclinés et non dérapés. Cependant, leur obsession du secret autour de leurs machines et de leurs capacités à réaliser un vol motorisé contrôlé (qu'ils maintiendront presque totalement jusqu'à l'obtention de brevet d'invention en 1905) entraînera un scepticisme général, en particulier en Europe, quand ils commenceront à communiquer en 1905 tout en exigeant un contrat commercial ferme avant toute démonstration. Cela explique le décalage de plusieurs années entre les premiers vols contrôlés de 1905, à l'écart de tout spectateur dans les dunes de Caroline du Nord, et les vols publics de 1908 où leur maîtrise du pilotage sera reconnue. Consacrant leur énergie à protéger leur invention et à des luttes de brevets, ils ne remettent pas en cause la configuration atypique de leur machine (configuration canard, pas d'ailerons, pas de roues, hélices à l'arrière), qui est obsolète en 1910 et ne sera pas poursuivie. <br/><br/><br/>
+</art1>
+  </div>
+</div>
+
+
+
+
+
+<script>
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+</script>
+</body>
+</html>
